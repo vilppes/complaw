ba0109480fa299c1a1c37011ffad973d2b857aa8
Pessi Vilppolahti <pessiv@outlook.com>
2022-11-25 21:49:34 +0000

    added todos

--- main.c
+++ main.c
@@ -1,2 +1,11 @@
-void main(){
-}
+//TODO: include the neccessary .h files
+//TODO: create the neccessary definitions
+//TODO: define the neccessary global variables
+//TODO: get finlex.fi data
+//TODO: parse finlex.fi data
+//TODO: simplify the finlex data
+//TODO: arrange the simplified data such that it is easy to read through
+//TODO: get user input for where they want to go in the data
+//TODO: provide results for the user input
+//TODO: export the simplified data / make exportable-
+void main(){}
